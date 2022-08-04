bf9169050e2b370482664acff66ab858f530e98a
Stanislav <tynkovski@gmail.com>
2022-08-04 20:57:18 +0400

    Tested private/public key builders

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,19 @@
 fun main() {
-    val publicKey       = Crypto.generateKeyPair().public
-    val string          = String(Crypto.Converter.toBase64(publicKey.encoded))
-    val secondPublicKey = Crypto.Builder.buildPublicKey(Crypto.Converter.fromBase64(string.toByteArray()))
-    val secondString    = String(Crypto.Converter.toBase64(secondPublicKey.encoded))
-    println("Original: $string")
-    println("Built   : $secondString")
+    val kp = Crypto.generateKeyPair()
+
+    val publicKey = kp.public
+    val publicKeyString = String(Crypto.Converter.toBase64(publicKey.encoded))
+    val secondPublicKey = Crypto.Builder.buildPublicKey(Crypto.Converter.fromBase64(publicKeyString.toByteArray()))
+    val secondPublicKeyString = String(Crypto.Converter.toBase64(secondPublicKey.encoded))
+
+    val privateKey = kp.private
+    val privateKeyString = String(Crypto.Converter.toBase64(privateKey.encoded))
+    val secondPrivateKey = Crypto.Builder.buildPrivateKey(Crypto.Converter.fromBase64(privateKeyString.toByteArray()))
+    val secondPrivateKeyString = String(Crypto.Converter.toBase64(secondPrivateKey.encoded))
+
+    println("Original: $publicKeyString")
+    println("Built   : $secondPublicKeyString")
+
+    println("Original: $privateKeyString")
+    println("Built   : $secondPrivateKeyString")
 }
