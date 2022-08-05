3a905e76147cfc7ff13f2f3f8977519e3de14aa0
Stanislav <tynkovski@gmail.com>
2022-08-05 14:59:41 +0400

    fix: user-friendly architecture

--- src/main/kotlin/com/tynkovski/cryptography/Crypto.kt
+++ src/main/kotlin/com/tynkovski/cryptography/Crypto.kt
@@ -5,51 +5,67 @@ import java.security.spec.PKCS8EncodedKeySpec
 import java.security.spec.X509EncodedKeySpec
 import java.util.*
 import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.spec.IvParameterSpec
 
 object Crypto {
-    private const val KEY_ALGORITHM = "RSA"
-    private const val SIGNATURE_ALGORITHM = "MD5withRSA"
+    object RSA {
+        const val KEY_ALGORITHM = "RSA"
+        private const val SIGNATURE_ALGORITHM = "MD5withRSA"
 
-    private val signature: Signature = Signature.getInstance(SIGNATURE_ALGORITHM)
-    private val cipher: Cipher = Cipher.getInstance(KEY_ALGORITHM)
-    private val keyGen: KeyPairGenerator = KeyPairGenerator.getInstance(KEY_ALGORITHM)
+        private val cipher: Cipher = Cipher.getInstance(KEY_ALGORITHM)
+        private val signature: Signature = Signature.getInstance(SIGNATURE_ALGORITHM)
+        private val keyGen: KeyPairGenerator = KeyPairGenerator.getInstance(KEY_ALGORITHM)
 
-    fun generateKeyPair(): KeyPair {
-        keyGen.initialize(1024)
-        return keyGen.genKeyPair()
-    }
+        fun generateKeyPair(): KeyPair {
+            keyGen.initialize(1024)
+            return keyGen.genKeyPair()
+        }
 
-    fun encrypt(data: ByteArray, key: PublicKey): ByteArray {
-        cipher.init(Cipher.ENCRYPT_MODE, key)
-        return cipher.doFinal(data)
-    }
+        fun encrypt(data: ByteArray, key: PublicKey): ByteArray {
+            cipher.init(Cipher.ENCRYPT_MODE, key)
+            return cipher.doFinal(data)
+        }
 
-    fun decrypt(data: ByteArray, key: PrivateKey): ByteArray {
-        cipher.init(Cipher.DECRYPT_MODE, key)
-        return cipher.doFinal(data)
-    }
+        fun decrypt(data: ByteArray, key: PrivateKey): ByteArray {
+            cipher.init(Cipher.DECRYPT_MODE, key)
+            return cipher.doFinal(data)
+        }
 
-    fun sign(data: ByteArray, privateKey: ByteArray): ByteArray {
-        signature.initSign(Builder.buildPrivateKey(privateKey))
-        signature.update(data)
-        return Converter.toBase64(signature.sign())
-    }
+        fun sign(data: ByteArray, privateKey: ByteArray): ByteArray {
+            signature.initSign(Builder.buildPrivateKey(privateKey))
+            signature.update(data)
+            return Converter.toBase64(signature.sign())
+        }
 
-    fun verify(data: ByteArray, publicKey: ByteArray, sign: ByteArray): Boolean {
-        signature.initVerify(Builder.buildPublicKey(publicKey))
-        signature.update(data)
-        return signature.verify(Converter.fromBase64(sign))
+        fun verify(data: ByteArray, publicKey: ByteArray, sign: ByteArray): Boolean {
+            signature.initVerify(Builder.buildPublicKey(publicKey))
+            signature.update(data)
+            return signature.verify(Converter.fromBase64(sign))
+        }
+
+        fun encryptToString(string: String, key: String): String {
+            val publicKey = Builder.buildPublicKey(key)
+            return encryptToString(string, publicKey)
+        }
+
+        fun decryptFromString(string: String, key: String): String {
+            val privateKey = Builder.buildPrivateKey(key)
+            return decryptFromString(string, privateKey)
+        }
+
+        fun encryptToString(string: String, key: PublicKey): String {
+            val encrypted = encrypt(string.toByteArray(), key)
+            return String(Converter.toBase64(encrypted))
+        }
+
+        fun decryptFromString(string: String, key: PrivateKey): String {
+            val decrypted = decrypt(Converter.fromBase64(string.toByteArray()), key)
+            return String(decrypted)
+        }
     }
 
     object Converter {
-        fun encryptToBase64String(string: String, key: PublicKey): String {
-            return String(toBase64(encrypt(string.toByteArray(), key)))
-        }
-
-        fun decryptFromBase64String(string: String, key: PrivateKey): String {
-            return String(decrypt(fromBase64(string.toByteArray()), key))
-        }
-
         fun toBase64(data: ByteArray): ByteArray {
             return Base64.getEncoder().encode(data)
         }
@@ -57,26 +73,34 @@ object Crypto {
         fun fromBase64(base64: ByteArray): ByteArray {
             return Base64.getDecoder().decode(base64)
         }
+
+        fun toString(base64: ByteArray): String {
+            return String(toBase64(base64))
+        }
+
+        fun fromString(string: String): ByteArray {
+            return fromBase64(string.toByteArray())
+        }
     }
 
     object Builder {
-        fun buildPrivateKey(stringInBase64: String): PrivateKey {
-            return buildPrivateKey(Converter.fromBase64(stringInBase64.toByteArray()))
+        fun buildPrivateKey(string: String): PrivateKey {
+            return buildPrivateKey(Converter.fromString(string))
         }
 
-        fun buildPublicKey(stringInBase64: String): PublicKey {
-            return buildPublicKey(Converter.fromBase64(stringInBase64.toByteArray()))
+        fun buildPublicKey(string: String): PublicKey {
+            return buildPublicKey(Converter.fromString(string))
         }
 
         fun buildPrivateKey(byteArray: ByteArray): PrivateKey {
             val keySpec = PKCS8EncodedKeySpec(byteArray)
-            val keyFactory = KeyFactory.getInstance(KEY_ALGORITHM)
+            val keyFactory = KeyFactory.getInstance(RSA.KEY_ALGORITHM)
             return keyFactory.generatePrivate(keySpec)
         }
 
         fun buildPublicKey(byteArray: ByteArray): PublicKey {
             val keySpec = X509EncodedKeySpec(byteArray)
-            val keyFactory = KeyFactory.getInstance(KEY_ALGORITHM)
+            val keyFactory = KeyFactory.getInstance(RSA.KEY_ALGORITHM)
             return keyFactory.generatePublic(keySpec)
         }
     }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,47 +1,25 @@
-import kotlin.system.measureTimeMillis
-
 import com.tynkovski.cryptography.Crypto
 
-data class Message(
-    var text: String,
-    val time: Long
-) {
-    companion object {
-        fun random(text: String): Message {
-            return Message(text, (0 until Long.MAX_VALUE).random())
-        }
-    }
-}
-
-fun randomString(): String {
+fun randomString(maxSize: Int): String {
     val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
-    return (1..32)
+    val size = (1..maxSize).random()
+    return (1..size)
         .map { kotlin.random.Random.nextInt(0, charPool.size) }
         .map(charPool::get)
         .joinToString("")
 }
 
 fun main() {
-    val kp = Crypto.generateKeyPair()
-    val messages = arrayListOf<Message>()
+    val data = "Hello, World!"
+    val key = Crypto.RSA.generateKeyPair()
 
-    repeat(10_000) {
-        messages.add(Message.random(randomString()))
-    }
+    val public = Crypto.Converter.toString(key.public.encoded)
+    println(public)
+    val encrypted = Crypto.RSA.encryptToString(data, public)
+    println(encrypted)
 
-    val time1 = measureTimeMillis {
-        messages.forEach { message ->
-            message.text = Crypto.Converter.encryptToBase64String(message.text, kp.public)
-        }
-    }
-
-    println("$time1 ms")
-
-    val time2 = measureTimeMillis {
-        messages.forEach { message ->
-            message.text = Crypto.Converter.decryptFromBase64String(message.text, kp.private)
-        }
-    }
-
-    println("$time2 ms")
+    val private = Crypto.Converter.toString(key.private.encoded)
+    println(private)
+    val decrypted = Crypto.RSA.decryptFromString(encrypted, private)
+    println(decrypted)
 }
