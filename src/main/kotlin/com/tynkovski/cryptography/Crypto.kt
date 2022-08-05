d907d5cb91e5823b5ba55d1b9d5efe38d840b570
Stanislav <tynkovski@gmail.com>
2022-08-05 16:08:04 +0400

    feat: aes256 encryption

--- src/main/kotlin/com/tynkovski/cryptography/Crypto.kt
+++ src/main/kotlin/com/tynkovski/cryptography/Crypto.kt
@@ -1,14 +1,35 @@
 package com.tynkovski.cryptography
 
+import java.util.Base64
 import java.security.*
-import java.security.spec.PKCS8EncodedKeySpec
-import java.security.spec.X509EncodedKeySpec
-import java.util.*
+import java.security.spec.*
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
 
 object Crypto {
+    object AES {
+        const val KEY_ALGORITHM = "AES"
+
+        private val cipher: Cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING")
+        private val keyGen: KeyGenerator = KeyGenerator.getInstance(KEY_ALGORITHM)
+
+        fun generateKey(): Key {
+            return keyGen.generateKey()
+        }
+
+        fun encrypt(data: ByteArray, key: Key): Pair<ByteArray, ByteArray> {
+            cipher.init(Cipher.ENCRYPT_MODE, key)
+            return Pair(cipher.doFinal(data), cipher.iv)
+        }
+
+        fun decrypt(data: ByteArray, key: Key, iv: ByteArray): ByteArray {
+            cipher.init(Cipher.DECRYPT_MODE, key, IvParameterSpec(iv))
+            return cipher.doFinal(data)
+        }
+    }
+
     object RSA {
         const val KEY_ALGORITHM = "RSA"
         private const val SIGNATURE_ALGORITHM = "MD5withRSA"
@@ -66,30 +87,42 @@ object Crypto {
     }
 
     object Converter {
+        /** Превращение любых данных в base64 массив */
         fun toBase64(data: ByteArray): ByteArray {
             return Base64.getEncoder().encode(data)
         }
 
+        /** Превращение base64 массива обратно в данные */
         fun fromBase64(base64: ByteArray): ByteArray {
             return Base64.getDecoder().decode(base64)
         }
 
-        fun toString(base64: ByteArray): String {
+        /** Превращение base64 массива в base64 строку */
+        fun toBase64String(base64: ByteArray): String {
             return String(toBase64(base64))
         }
 
-        fun fromString(string: String): ByteArray {
+        /** Превращение base64 строки в base64 массив */
+        fun fromBase64String(string: String): ByteArray {
             return fromBase64(string.toByteArray())
         }
     }
 
     object Builder {
+        fun buildKey(string: String): SecretKeySpec {
+            return buildKey(Converter.fromBase64String(string))
+        }
+
+        fun buildKey(byteArray: ByteArray): SecretKeySpec {
+            return SecretKeySpec(byteArray, AES.KEY_ALGORITHM)
+        }
+
         fun buildPrivateKey(string: String): PrivateKey {
-            return buildPrivateKey(Converter.fromString(string))
+            return buildPrivateKey(Converter.fromBase64String(string))
         }
 
         fun buildPublicKey(string: String): PublicKey {
-            return buildPublicKey(Converter.fromString(string))
+            return buildPublicKey(Converter.fromBase64String(string))
         }
 
         fun buildPrivateKey(byteArray: ByteArray): PrivateKey {
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -13,13 +13,28 @@ fun main() {
     val data = "Hello, World!"
     val key = Crypto.RSA.generateKeyPair()
 
-    val public = Crypto.Converter.toString(key.public.encoded)
+    val public = Crypto.Converter.toBase64String(key.public.encoded)
     println(public)
     val encrypted = Crypto.RSA.encryptToString(data, public)
     println(encrypted)
 
-    val private = Crypto.Converter.toString(key.private.encoded)
+    val private = Crypto.Converter.toBase64String(key.private.encoded)
     println(private)
     val decrypted = Crypto.RSA.decryptFromString(encrypted, private)
     println(decrypted)
+
+    // --
+
+    val data2 = "Hello, AES!"
+    val key2 = Crypto.AES.generateKey()
+
+    val aesKeyString = Crypto.Converter.toBase64String(key2.encoded)
+    println(aesKeyString)
+    val builtKey = Crypto.Builder.buildKey(aesKeyString)
+
+    val (encryptedAes, initVector) = Crypto.AES.encrypt(data2.toByteArray(), builtKey)
+    println(Crypto.Converter.toBase64String(encryptedAes))
+
+    val decryptedAes = Crypto.AES.decrypt(encryptedAes, key2, initVector)
+    println(String(decryptedAes))
 }
