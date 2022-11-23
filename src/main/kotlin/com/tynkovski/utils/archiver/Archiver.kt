b621959ee7fcd4fd9d8d4787ce5f46cac35b773e
Stanislav <tynkovski@gmail.com>
2022-11-24 00:53:22 +0400

    feat: refactoring

--- src/main/kotlin/com/tynkovski/utils/archiver/Archiver.kt
+++ src/main/kotlin/com/tynkovski/utils/archiver/Archiver.kt
@@ -1,17 +1,15 @@
 package com.tynkovski.utils.archiver
 
-import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import java.util.zip.GZIPInputStream
 import java.util.zip.GZIPOutputStream
 
 object Archiver {
-    fun zip(content: String): ByteArray {
-        val bos = ByteArrayOutputStream()
-        GZIPOutputStream(bos).bufferedWriter().use { it.write(content) }
-        return bos.toByteArray()
-    }
+    fun zip(content: ByteArray): ByteArray =
+        ByteArrayOutputStream().also { bos ->
+            GZIPOutputStream(bos).buffered().use { it.write(content) }
+        }.toByteArray()
 
-    fun unzip(content: ByteArray): String =
-        GZIPInputStream(content.inputStream()).bufferedReader().use { it.readText() }
+    fun unzip(content: ByteArray): ByteArray =
+        GZIPInputStream(content.inputStream()).buffered().use { it.readBytes() }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,12 +2,17 @@ import com.tynkovski.utils.archiver.Archiver
 import com.tynkovski.utils.extentions.times
 
 fun main() {
-    val data = listOf(
+    val data = (listOf(
         "a"
-    ) * 1000
+    ) * 1000).joinToString("")
+
     println(data)
-    val archived = Archiver.zip(data.joinToString(""))
+
+    val archived = Archiver.zip(data.toByteArray())
     println(archived.size)
+    println(String(archived))
+
     val unarchived = Archiver.unzip(archived)
-    println(unarchived.length)
+    println(unarchived.size)
+    println(String(unarchived))
 }
